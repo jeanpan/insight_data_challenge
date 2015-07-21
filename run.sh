01007ee134a981e8cf78d51df7a5a60d3d85a308
jeanpan <beats.1213@gmail.com>
2015-07-20 22:05:27 -0400

    update comment

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 # example of the run script for running the word count
 cat ./tweet_input/tweets.txt | ./src/mapper.py | ./src/reducer.py > ./tweet_output/ft1.txt
-# example of the run script for running the word count
+
+# example of the run script for running the streaming median
 cat ./tweet_input/tweets.txt | ./src/median.py > ./tweet_output/ft2.txt
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
